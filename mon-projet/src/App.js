bd29a79c1880d70d8506197fc0a38189ffa8c89e
Archad-prog <ouedarchad@gmail.com>
2025-03-11 22:55:40 +0000

    Ajout Boostrap

--- mon-projet/src/App.js
+++ mon-projet/src/App.js
@@ -1,27 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-function App() {
+// etudiant contient la liste des etudiants recuperée des 
+// mesEtudiants permet de mettre a jour lesEtudiants
+export default function App() {
+  const [lesEtudiants, mesEtudiants] = useState([]);
+
+  // Charger la liste des étudiants depuis l'API
+  useEffect(() => {
+    fetch("http://localhost:8888/SGA/api.php")
+      .then((res) => res.json()) // Correction ici
+      .then((data) => mesEtudiants(data)) // Mettre à jour l'état avec les données
+      .catch((error) => console.error("Erreur lors de la récupération des étudiants :", error));
+  }, []); // Le tableau vide signifie que l'effet ne s'exécute qu'au montage du composant
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1> Bonjour a tout le monde</h1>
-      
-      </header>
+    <div>
+      <h1>Liste des Étudiants</h1>
+      <ul>
+        {lesEtudiants.map((etudiant, index) => (
+          <li key={index}>{etudiant.nom} - Absences : {etudiant.nbrAbs}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
--- mon-projet/src/index.js
+++ mon-projet/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const myfirstelement=<h1> Hello World</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
-  myfirstelement
-);
+  <App/>
+);                         
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
